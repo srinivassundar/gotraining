f9862b281027d9d60c2f62c43f517d23dd82274b
srinivas <srinivas.sundar@walmartlabs.com>
2020-09-28 23:08:32 +0530

    added println return values

--- module1/srinivas.go
+++ module1/srinivas.go
@@ -12,6 +12,8 @@ func main() {
 		fmt.Println(i)
 	}
 	bar()
+
+	returnvalues()
 }
 
 func foo() {
@@ -21,5 +23,13 @@ func foo() {
 
 func bar() {
 	fmt.Println("and then we exited")
+
+}
+
+func returnvalues() {
+
+	n, err := fmt.Println("this is an amazing piece of code")
+	fmt.Println("these the bytes written", n)
+	fmt.Println("the error code is", err)
 
 }
